9ba0d0eee92d2b3ea5c3b1d490675985a53bb04c
angel munoz <angel.munoz@tokiota.com>
2019-06-10 16:42:29 +0200

    create modules and new lazy loading angular 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import(`./home/home.module`).then(m => m.HomeModule),
+  },
+  {
+    path: 'test',
+    loadChildren: () => import(`./test/test.module`).then(m => m.TestModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
